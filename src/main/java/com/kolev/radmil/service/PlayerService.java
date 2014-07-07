5cbebe454a644cbd72b78cce1264bbebbaadeade
Raduncho <radmil.kolev@gmail.com>
2014-07-07 22:23:52 +0300

    embedded Tomcat server

--- src/main/java/com/kolev/radmil/service/PlayerService.java
+++ src/main/java/com/kolev/radmil/service/PlayerService.java
@@ -2,11 +2,37 @@ package com.kolev.radmil.service;
 
 import java.util.List;
 
-import com.kolev.radmil.entity.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface PlayerService {
-	public void addPlayer(Player player);
-	public void removePlayer(int cardId);
-	public Player getPlayer(int cardId);
-	public List getAllPlayers();
+import com.kolev.radmil.entity.Player;
+import com.kolev.radmil.repository.PlayerRepository;
+
+@Service
+public class PlayerService {
+
+	@Autowired
+	private PlayerRepository repository;
+
+	@Transactional
+	public void addPlayer(Player player) {
+		repository.save(player);
+	}
+
+	@Transactional
+	public void removePlayer(int cardId) {
+		repository.delete(cardId);
+	}
+
+	@Transactional
+	public Player getPlayer(int cardId) {
+		return repository.findOne(cardId);
+	}
+
+	@Transactional
+	public List getAllPlayers() {
+		return repository.findAll();
+	}
+
 }
